be38c5716cc9fc1bf518659319341792cfac939c
ryan <itsalaidbacklife@gmail.com>
2016-03-15 07:58:38 +0100

    Dynamic animations with ng-class

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ app.controller("tutorialController", function($scope, toastr) {
 	this.tab = 'bob';
 	this.sets = [];
 	this.elements = [];
-	this.elStyle = "{'-webkit-animation': 'fading 4s infinite', 'animation': 'fading 4s infinite'}";
+	this.elFlash = true;
+	this.bobFlash = false;
+	// this.elStyle = "{'-webkit-animation': 'fading 4s infinite', 'animation': 'fading 4s infinite'}";
 
 
 	var a = new Set("sets", "A");
@@ -16,7 +18,9 @@ app.controller("tutorialController", function($scope, toastr) {
 	this.dragEl = function (ev) {
 		if ($scope.tut.tab === 'bob' && ev.target.getAttribute('id') === 'x') {
 			console.log("Dragging x while displaying Bob");
-			$scope.tut.elStyle = "{'-webkit-animation': 'fading 0s', 'animation': 'fading 0s'}";
+			// $scope.tut.elStyle = "{'-webkit-animation': 'fading 0s', 'animation': 'fading 0s'}";
+			this.bobFlash = true;
+			this.elFlash = false;
 			$scope.$apply();
 		}
 	};

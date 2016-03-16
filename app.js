930d9624ce598b2c075216fa74c2e8b293d06b66
ryan <itsalaidbacklife@gmail.com>
2016-03-16 21:44:56 +0100

    2nd step in tutorial. Now needs promises for asynchronous toast

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var dragData = {
 	index: null
 };
 
+openToasts = [];
+
 app.config(function(toastrConfig) {
 	angular.extend(toastrConfig, {
 		timeOut: 0
@@ -21,7 +23,6 @@ app.directive('droppableBob', function() {
 			el.addEventListener(
 				'dragover', 
 				function(e){
-					console.log("dragover");
 					if (dragData.id === 'x') {
 						e.preventDefault();
 					}
@@ -51,6 +52,48 @@ app.directive('droppableBob', function() {
 	}	//return
 });	//droppable directive
 
+app.directive('droppableSets', function() {
+	return {
+		scope: {
+			drop: '&',
+			dragover: '&'
+		},
+		link: function (scope, element) {
+			var el = element[0];
+
+			el.addEventListener(
+				'dragover', 
+				function(e){
+					var id = el.getAttribute('id');
+					var fn = scope.dragover();
+					var steps = fn();
+					if (dragData.id === 'bob' && steps === 1) e.preventDefault();
+					return false;
+				},
+				false
+			);
+
+			el.addEventListener(
+				'drop', function(e) {
+					scope.$apply(function(scope) {
+						var fn = scope.drop();
+						var id = e.target.id;
+						if('undefined' !== typeof fn) {
+							fn(id);
+						}
+						dragData = {
+							id: '',
+							index: null
+						};
+					});
+					return false;
+				},
+				false
+			);
+		}	//link
+	}	//return	
+});
+
 app.controller("tutorialController", function($scope, toastr) {
 	this.completeSteps = 0;
 	this.tab = 'bob';
@@ -59,6 +102,7 @@ app.controller("tutorialController", function($scope, toastr) {
 	this.selectedElements = [];
 	this.elFlash = true;
 	this.bobFlash = false;
+	this.setsFlash = false;
 	// this.elStyle = "{'-webkit-animation': 'fading 4s infinite', 'animation': 'fading 4s infinite'}";
 
 
@@ -72,24 +116,52 @@ app.controller("tutorialController", function($scope, toastr) {
 		$scope.tut.completeSteps = 1;
 		$scope.tut.selectedElements.push($scope.tut.elements.splice(0, 1)[0]);
 		$scope.tut.bobFlash = false;
-		toastr.success('Every element is in some sets, and not in others.', "Nice!", {
+		console.log(openToasts[0]);
+		toastr.clear(openToasts.pop());;
+		openToasts.push(toastr.success('Every element is in some sets, and not in others.', "Nice!", {
 			onHidden: function(clicked) {
-				toastr.info('Now conjur Bob into existence, by dragging him to the set area');
+				openToasts.pop();
+				console.log("Gonna push conjur toast");
+				openToasts.push(toastr.info('Now conjur Bob into existence, by dragging him to the set area', 
+					{
+						extendedTimeOut: 8000,
+						// tapToDismiss: false
+					}));
+				console.log(openToasts);
+				$scope.tut.bobFlash = true;
 			}
-		});
+		}));
 	};
 
+	$scope.dropIntoSets = function (id) {
+		switch ($scope.tut.completeSteps) {
+			//Dragging bob into sets
+			case 1:
+				if (dragData.id === 'bob') {
+					$scope.tut.completeSteps = 2;
+					var caughtToast = openToasts.pop();
+					toastr.clear(caughtToast);
+					if (caughtToast.toastId < 2) toastr.clear(openToasts.pop());
+					openToasts.push(toastr.success("Now Bob exists, and he has x inside him. He is forever grateful.", "Congratulations!"));
+					var bob = new Set("sets", "Bob");
+					bob.putIn(x);
+					$scope.tut.sets.push(bob);
+					$scope.tut.tab = '';
+					$scope.tut.elements.push($scope.tut.selectedElements.splice(0, 1)[0]);
+
+				}
+				break;
+		}
+	}
 
 	this.dragEl = function (ev) {
 		if ($scope.tut.tab === 'bob' && ev.target.getAttribute('id') === 'x') {
-			console.log("Dragging x while displaying Bob");
 			switch ($scope.tut.completeSteps) {
 				case 0:
 					this.bobFlash = true;
 					this.elFlash = false;
 					dragData.id = ev.target.getAttribute('id');
 					dragData.index = ev.target.getAttribute('index');
-					console.log(dragData);
 					break;
 			}
 			$scope.$apply();
@@ -104,5 +176,27 @@ app.controller("tutorialController", function($scope, toastr) {
 		}
 	}
 
-	toastr.info('Drag x into Bob', 'Welcome =)');
+	this.dragBob = function (ev) {
+		dragData.id = ev.target.id;
+		dragData.index = null;
+		if ($scope.tut.completeSteps === 1) {
+			$scope.tut.setsFlash = true;
+			$scope.$apply();
+		}
+		
+	}
+
+	this.endDragBob = function (ev) {
+		$scope.tut.setsFlash = false;
+		if ($scope.tut.completeSteps === 1) {
+			$scope.tut.bobFlash = true;
+		}
+		$scope.$apply();
+	}
+
+	$scope.dragOverSets = function () {
+		return $scope.tut.completeSteps;
+	}
+
+	openToasts.push(toastr.info('Drag x into Bob', 'Welcome =)'));
 });
